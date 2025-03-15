7537ab56a23f8d032597c6091afc3bc78f5f33f3
JonnyPhang <jonny_pyj@hotmail.com>
2025-03-15 14:47:28 +0800

    improve the response time

--- app_config.py
+++ app_config.py
@@ -20,6 +20,8 @@ class Configuration:
         corpus_name = config.get('Corpus-Med', 'name')
         corpus_medical_dir = config.get('Corpus-Med', 'medical_dir')
         corpus_medical_name = config.get('Corpus-Med', 'medical_name')
+        corpus_medical_freq_dir = config.get('Corpus-Med', 'medical_freq_dir')
+        corpus_medical_freq_name = config.get('Corpus-Med', 'medical_freq_name')
 
         # Return a dictionary with the retrieved values
         self.config_values = {
@@ -27,5 +29,7 @@ class Configuration:
             'app_data': app_data,
             'corpus_name': corpus_name,
             'corpus_medical_dir': corpus_medical_dir,
-            'corpus_medical_name': corpus_medical_name
+            'corpus_medical_name': corpus_medical_name,
+            'corpus_medical_freq_dir': corpus_medical_freq_dir,
+            'corpus_medical_freq_name': corpus_medical_freq_name
         }
